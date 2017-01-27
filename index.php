50e7be0eb9c75615e882d3a8894bffa093e64f38
Artem Zinoviev <arzinoviev@gmail.com>
2017-01-27 10:05:20 +0300

    add button style

--- index.php
+++ index.php
@@ -50,6 +50,44 @@
         a {
             cursor: pointer;
         }
+
+        .auth--form__button {
+            -moz-box-shadow:inset 0px 1px 0px 0px #9fb4f2;
+            -webkit-box-shadow:inset 0px 1px 0px 0px #9fb4f2;
+            box-shadow:inset 0px 1px 0px 0px #9fb4f2;
+            background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #7892c2), color-stop(1, #476e9e));
+            background:-moz-linear-gradient(top, #7892c2 5%, #476e9e 100%);
+            background:-webkit-linear-gradient(top, #7892c2 5%, #476e9e 100%);
+            background:-o-linear-gradient(top, #7892c2 5%, #476e9e 100%);
+            background:-ms-linear-gradient(top, #7892c2 5%, #476e9e 100%);
+            background:linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
+            filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#7892c2', endColorstr='#476e9e',GradientType=0);
+            background-color:#7892c2;
+            border:1px solid #4e6096;
+            display:inline-block;
+            cursor:pointer;
+            color:#ffffff;
+            font-family:Arial;
+            font-size:14px;
+            font-weight:bold;
+            padding:5px 32px;
+            text-decoration:none;
+            text-shadow:0px 1px 0px #283966;
+        }
+        .auth--form__button:hover {
+            background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #476e9e), color-stop(1, #7892c2));
+            background:-moz-linear-gradient(top, #476e9e 5%, #7892c2 100%);
+            background:-webkit-linear-gradient(top, #476e9e 5%, #7892c2 100%);
+            background:-o-linear-gradient(top, #476e9e 5%, #7892c2 100%);
+            background:-ms-linear-gradient(top, #476e9e 5%, #7892c2 100%);
+            background:linear-gradient(to bottom, #476e9e 5%, #7892c2 100%);
+            filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#476e9e', endColorstr='#7892c2',GradientType=0);
+            background-color:#476e9e;
+        }
+        .auth--form__button:active {
+            position:relative;
+            top:1px;
+        }
     </style>
 </head>
 <body>
@@ -69,10 +107,10 @@
 
                     <div>
                         <div class="btn">
-                            <button type="submit">login</button>
+                            <button class="auth--form__button" type="submit">login</button>
                         </div>
                         <div class="btn">
-                            <a class="button" href="?register">Don't have account?</a>
+                            <a href="?register">Don't have account?</a>
                         </div>
                     </div>
                 </form>
@@ -93,10 +131,10 @@ LOGIN;
 
                     <div>
                         <div class="btn">
-                            <button type="submit">register</button>
+                            <button class="auth--form__button" type="submit">register</button>
                         </div>
                         <div class="btn">
-                            <a class="button" href="?login">Back to login.</a>
+                            <a href="?login">Back to login.</a>
                         </div>
                     </div>
                 </form>
